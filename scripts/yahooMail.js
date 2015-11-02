1793b7ec2c4483752a53c158ba716c5f69e4f700
nibzo1 <nibzo1@yahoo.co.uk>
2015-11-02 19:30:12 +0000

    updates for updated ymail markup

--- scripts/yahooMail.js
+++ scripts/yahooMail.js
@@ -1,6 +1,32 @@
-$('#theAd').remove();
-$('#yucs').css('max-width','100%');
-$('#main').css('max-width','100%');
-$('#yuhead-bucket').css('max-width','100%');
-$('#shellcontent').css('right','0px');
-$('#shellcontent').removeClass('wide-right-rail');
+function changeUI() {
+
+  $('#uhWrapper').css('padding-top','22px');
+  $('#masterNav').remove();
+  removeListAdd();
+  $('#theAd').remove();
+  $('#yucs').css('max-width','100%');
+  $('#main').css('max-width','100%');
+  $('.frost-container').css('right','0');
+  $('#yuhead-bucket').css('max-width','100%');
+  $('#shellcontent').css('right','0px');
+  $('#shellcontent').removeClass('wide-right-rail');
+}
+changeUI();
+
+$(document).ready(function() {
+  changeUI();
+});
+
+$(document).ajaxSuccess(function() {
+  changeUI();
+});
+
+$('body').bind('DOMSubtreeModified', function() {
+  if ($('.mb-list-ad').length) {
+    removeListAdd();
+  }
+});
+
+function removeListAdd() {
+  $('.mb-list-ad').remove();
+}
