42c6e950e5dfbc173daf630b756a9022d13fe068
nibzo1 <nibzo1@yahoo.co.uk>
2015-11-03 11:22:24 +0000

    jQuery no conflict

--- scripts/yahooMail.js
+++ scripts/yahooMail.js
@@ -1,27 +1,39 @@
+var YN = jQuery.noConflict();
+
+// Main page cleanup
 function changeUI() {
-  $('#uhWrapper').css('padding-top','22px');
-  $('#masterNav, #theAd').remove();
+  YN('#uhWrapper').css('padding-top','22px');
+  YN('#masterNav, #theAd').remove();
   removeListAdd();
-  $('#main, #yucs, #yuhead-bucket').css('max-width','100%');
-  $('#shellcontent, .frost-container').css('right','0px');
-  $('#shellcontent').removeClass('wide-right-rail');
+  YN('#main, #yucs, #yuhead-bucket').css('max-width','100%');
+  YN('#shellcontent, .frost-container').css('right','0px');
+  YN('#shellcontent').removeClass('wide-right-rail');
 }
 
+// remove list adds
 function removeListAdd() {
-  $('.mb-list-ad, .search-message-container-ad').remove();
+  YN('.mb-list-ad, .search-message-container-ad').remove();
 }
 
-
+// Init page cleanup
 changeUI();
 
-$(document).ready(function() {
+// Listen for page to load
+YN(window).load(function() {
   changeUI();
 });
 
-$(document).ajaxSuccess(function() {
+// Listen for page to finnish
+YN(document).ready(function() {
   changeUI();
 });
 
-$('body').bind('DOMSubtreeModified', function() {
+// Listen for complete AJAX calls
+YN(document).ajaxSuccess(function() {
+  changeUI();
+});
+
+// Bind to changes in the DOM
+YN('body').bind('DOMSubtreeModified', function() {
     removeListAdd();
 });
